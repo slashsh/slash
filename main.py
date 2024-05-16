109bd8a4ece09930028516b5533dd3fe71656dda
Butterroach <butterroach@proton.me>
2024-05-16 03:59:36 +0300

    fix

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ os.system("")
 
 linec = 0
 
-__version__ = "2.1.0-alpha"
+__version__ = "2.2.0-alpha"
 
 
 class SlangError(Exception):
@@ -156,8 +156,12 @@ def process_single_line(
     if wompwomp:
         raise NoSuchFunction(line.split(" ")[0])
 
-def penis(string: string)
-return "penis"
+
+def penis(string: str):
+    """
+    thank you ocaminty
+    """
+    return "penis"
 
 
 def add(x: float, y: float):
@@ -264,6 +268,7 @@ functions = [
         lambda x: eval(x),
         "Evaluates a Python expression using Python's built-in eval.",
     ),
+    ("penis", penis, "GRAAAAAAAHHHHH ONLY SE STR PLASE LR ELSE I WILL KILL YOUPLEAS DONT EJIN THE TUN"),
 ]
 
 
